096d13f0340a9462dd24ec4cb5ae9a933cc2cbbd
teamkotlin <muhammadramzan6254@gmail.com>
2023-09-05 02:30:40 +0500

    working on home page

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -560,7 +560,8 @@
                                 <div class="aa-promo-right">
                                     <div class="aa-single-promo-right">
                                         <div class="aa-promo-banner">
-                                            <img src="img/promo-banner-3.jpg" alt="img" />
+                                            <img src="{{ asset('home_assets/img/promo-banner-3.jpg') }}"
+                                                alt="img" />
                                             <div class="aa-prom-content">
                                                 <span>Exclusive Item</span>
                                                 <h4>
@@ -1951,7 +1952,8 @@
                                         <li>
                                             <figure>
                                                 <a class="aa-product-img" href="#"><img
-                                                        src="img/women/girl-4.png" alt="polo shirt img" /></a>
+                                                        src="{{ asset('home_assets/img/women/girl-4.png') }}"
+                                                        alt="polo shirt img" /></a>
                                                 <a class="aa-add-card-btn" href="#"><span
                                                         class="fa fa-shopping-cart"></span>Add To Cart</a>
                                                 <figcaption>
@@ -2581,7 +2583,8 @@
                             <!-- single slide -->
                             <li>
                                 <div class="aa-testimonial-single">
-                                    <img class="aa-testimonial-img" src="img/testimonial-img-2.jpg"
+                                    <img class="aa-testimonial-img"
+                                        src="{{ asset('home_assets/img/testimonial-img-2.jpg') }}"
                                         alt="testimonial img" />
                                     <span class="fa fa-quote-left aa-testimonial-quote"></span>
                                     <p>
@@ -2602,7 +2605,8 @@
                             <!-- single slide -->
                             <li>
                                 <div class="aa-testimonial-single">
-                                    <img class="aa-testimonial-img" src="img/testimonial-img-1.jpg"
+                                    <img class="aa-testimonial-img"
+                                        src="{{ asset('home_assets/img/testimonial-img-1.jpg') }}"
                                         alt="testimonial img" />
                                     <span class="fa fa-quote-left aa-testimonial-quote"></span>
                                     <p>
@@ -2623,7 +2627,8 @@
                             <!-- single slide -->
                             <li>
                                 <div class="aa-testimonial-single">
-                                    <img class="aa-testimonial-img" src="img/testimonial-img-3.jpg"
+                                    <img class="aa-testimonial-img"
+                                        src="{{ asset('home_assets/img/testimonial-img-3.jpg') }}"
                                         alt="testimonial img" />
                                     <span class="fa fa-quote-left aa-testimonial-quote"></span>
                                     <p>
@@ -2661,7 +2666,9 @@
                             <div class="col-md-4 col-sm-4">
                                 <div class="aa-latest-blog-single">
                                     <figure class="aa-blog-img">
-                                        <a href="#"><img src="img/promo-banner-1.jpg" alt="img" /></a>
+                                        <a href="#"><img
+                                                src="{{ asset('home_assets/img/promo-banner-1.jpg') }}"
+                                                alt="img" /></a>
                                         <figcaption class="aa-blog-img-caption">
                                             <span href="#"><i class="fa fa-eye"></i>5K</span>
                                             <a href="#"><i class="fa fa-thumbs-o-up"></i>426</a>
@@ -2693,7 +2700,9 @@
                             <div class="col-md-4 col-sm-4">
                                 <div class="aa-latest-blog-single">
                                     <figure class="aa-blog-img">
-                                        <a href="#"><img src="img/promo-banner-3.jpg" alt="img" /></a>
+                                        <a href="#"><img
+                                                src="{{ asset('home_assets/img/promo-banner-3.jpg') }}"
+                                                alt="img" /></a>
                                         <figcaption class="aa-blog-img-caption">
                                             <span href="#"><i class="fa fa-eye"></i>5K</span>
                                             <a href="#"><i class="fa fa-thumbs-o-up"></i>426</a>
@@ -2725,7 +2734,9 @@
                             <div class="col-md-4 col-sm-4">
                                 <div class="aa-latest-blog-single">
                                     <figure class="aa-blog-img">
-                                        <a href="#"><img src="img/promo-banner-1.jpg" alt="img" /></a>
+                                        <a href="#"><img
+                                                src="{{ asset('home_assets/img/promo-banner-1.jpg') }}"
+                                                alt="img" /></a>
                                         <figcaption class="aa-blog-img-caption">
                                             <span href="#"><i class="fa fa-eye"></i>5K</span>
                                             <a href="#"><i class="fa fa-thumbs-o-up"></i>426</a>
@@ -2769,38 +2780,51 @@
                     <div class="aa-client-brand-area">
                         <ul class="aa-client-brand-slider">
                             <li>
-                                <a href="#"><img src="img/client-brand-java.png" alt="java img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-java.png') }}"
+                                        alt="java img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-jquery.png" alt="jquery img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-jquery.png') }}"
+                                        alt="jquery img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-html5.png" alt="html5 img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-html5.png') }}"
+                                        alt="html5 img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-css3.png" alt="css3 img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-css3.png') }}"
+                                        alt="css3 img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-wordpress.png"
+                                <a href="#"><img
+                                        src="{{ asset('home_assets/img/client-brand-wordpress.png') }}"
                                         alt="wordPress img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-joomla.png" alt="joomla img" /></a>
+                                <a href="#"><img
+                                        src="{{ asset('home_assets/img/client-brand-joomla.png') }}"
+                                        alt="joomla img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-java.png" alt="java img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-java.png') }}"
+                                        alt="java img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-jquery.png" alt="jquery img" /></a>
+                                <a href="#"><img
+                                        src="{{ asset('home_assets/img/client-brand-jquery.png') }}"
+                                        alt="jquery img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-html5.png" alt="html5 img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-html5.png') }}"
+                                        alt="html5 img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-css3.png" alt="css3 img" /></a>
+                                <a href="#"><img src="{{ asset('home_assets/img/client-brand-css3.png') }}"
+                                        alt="css3 img" /></a>
                             </li>
                             <li>
-                                <a href="#"><img src="img/client-brand-wordpress.png"
+                                <a href="#"><img
+                                        src="{{ asset('home_assets/img/client-brand-wordpress.png') }}"
                                         alt="wordPress img" /></a>
                             </li>
                         </ul>
